ab16fa1ba1f870c8cdc8696b0fbf5ec41dfcaf5e
crazydevnew <tel123404@gmail.com>
2023-05-19 13:08:52 +0500

    Added comments main.py

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import json
 
 
 def element_to_dict(element):
+    # Имя и атрибуты элемента
     result = {'name': element.tag, 'attributes': {}}
 
     for name, value in element.attrib.items():
+        print(name, value)
         result['attributes'][name] = value
 
+    # Дочерние элементы
     children = list(element)
     if children:
         result['children'] = [element_to_dict(child) for child in children]
@@ -17,12 +20,15 @@ def element_to_dict(element):
     return result
 
 
-with open('schedule.xml', 'r') as file:
-    root = ET.fromstring(file.read())
+# Чтение schedule.xml
+root = ET.parse("schedule.xml").getroot()
 
+# Создание словаря для корневого элемента
 xml_dict = element_to_dict(root)
 
+# Сериализация словаря в JSON
 json_data = json.dumps(xml_dict, indent=4)
 
+# Запись JSON-данных в файл
 with open('schedule.json', 'w') as file:
     file.write(json_data)
